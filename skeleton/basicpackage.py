6fedffbb91c0e66e57fa88e98e840dc5a909c6c4
Damien Lebrun <dinoboff@hotmail.com>
2010-04-30 03:25:08 +0100

    add a Skeleton for basic python package.

---
+++ skeleton/basicpackage.py
@@ -0,0 +1,41 @@
+from __future__ import with_statement
+
+import os
+
+from skeleton import Skeleton, Var
+
+
+NS_HEADER = """
+__import__('pkg_resources').declare_namespace(__name__)
+"""
+
+class BasicPackage(Skeleton):
+    src = 'basic-package'
+    vars = [
+        Var('ProjectName'),
+        Var('PackageName'),
+        Var('Author'),
+        Var('AuthorEmail')
+        ]
+        
+    def pre_write(self, dst_dir):
+        self['NSPackages'] = []
+        self['Packages'] = []
+        
+        # Create namespace packages
+        current_ns = []
+        package_path = self['PackageName'].split('.')
+        for p in package_path[:-1]:
+            current_ns.append(p)
+            ns = '.'.join(current_ns)
+            self['NSPackages'].append(ns)
+            self['Packages'].append(ns)
+            self._create_package(os.path.join(dst_dir, *current_ns), NS_HEADER)
+        
+        # Create package 
+        self._create_package(os.path.join(dst_dir, *package_path))
+    
+    def _create_package(self, path, init_body=''):
+        os.mkdir(path)
+        with open(os.path.join(path, '__init__.py'), 'w') as f:
+            f.write(init_body)
---
+++ skeleton/tests/testbasicpackage.py
@@ -0,0 +1,41 @@
+import os
+import unittest
+
+from mock import patch
+
+from skeleton.basicpackage import BasicPackage
+from skeleton.tests.testskeleton import TempDir
+
+
+class TestBasicPackage(unittest.TestCase):
+    
+    def setUp(self):
+        super(TestBasicPackage, self).setUp()
+        self.tmp_dir = TempDir()
+        self.tmp_dir.create()
+        
+    def tearDown(self):
+        super(TestBasicPackage, self).tearDown()
+        self.tmp_dir.remove()
+
+    def test_write(self):
+        vars = {
+            'ProjectName': 'foo',
+            'PackageName': 'foo',
+            'Author': 'Damien Lebrun',
+            'AuthorEmail': 'dinoboff@gmail.com',
+            }
+        s = BasicPackage(vars)
+        s.write(self.tmp_dir.path)
+        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'distribute_setup.py')))
+        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'MANIFEST.in')))
+        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'README.rst')))
+        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'setup.py')))
+        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'foo/__init__.py')))
+
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(TestBasicPackage)
+
+if __name__ == "__main__":
+    unittest.main()
