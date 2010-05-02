24b144d2e52f51632b4578892deda31300b11f72
Damien Lebrun <dinoboff@hotmail.com>
2010-05-02 05:20:49 +0100

    Template download nightly distribute_setup.py
    Instead of including the distribute_setup.py, the Skeleton will
    download it.

--- skeleton/examples/basicpackage.py
+++ skeleton/examples/basicpackage.py
@@ -1,15 +1,39 @@
 from __future__ import with_statement
 
 import os
+import urllib
 
 from skeleton import Skeleton, Var
+from skeleton.utils import get_loggger
+import logging
 
+
+log = get_loggger(__name__)
 
 NS_HEADER = """
 __import__('pkg_resources').declare_namespace(__name__)
 """
 
+DISTRIBUTE_SETUP_URL = 'http://nightly.ziade.org/distribute_setup.py'
+
 class BasicPackage(Skeleton):
+    """
+    Create a new package package (with namespace support) with the setup.py,
+    README.rst and MANIFEST.in files already setup.
+    
+    Require the following variables:
+    
+    - ProjectName;
+    - PackageName;
+    - Author;
+    - and AuthorEmail.
+    
+    Todo: Allow to set defaults for AUthor and AuthorEmail.
+    Todo: Better README.rst content like Having a basic Installation and
+    requirement section.
+    Todo: Setup a test package and the distribute use_2to3 option.
+    """
+    
     src = 'basic-package'
     vars = [
         Var('ProjectName'),
@@ -19,6 +43,10 @@ class BasicPackage(Skeleton):
         ]
         
     def pre_write(self, dst_dir):
+        self._create_package_and_namespaces(dst_dir)
+        self._get_distribute(dst_dir)
+    
+    def _create_package_and_namespaces(self, dst_dir):
         self['NSPackages'] = []
         self['Packages'] = []
         
@@ -30,15 +58,26 @@ class BasicPackage(Skeleton):
             ns = '.'.join(current_ns)
             self['NSPackages'].append(ns)
             self['Packages'].append(ns)
-            self._create_package(os.path.join(dst_dir, *current_ns), NS_HEADER)
+            self._create_package(dst_dir, current_ns, NS_HEADER)
         
         # Create package 
-        self._create_package(os.path.join(dst_dir, *package_path))
+        self._create_package(dst_dir, package_path)
     
-    def _create_package(self, path, init_body=''):
+    def _create_package(self, dst_dir, package_part, init_body=''):
+        log.info("Creating package %s" % '.'.join(package_part))
+        if self.run_dry:
+            return
+        path = os.path.join(dst_dir, *package_part)
         os.mkdir(path)
         with open(os.path.join(path, '__init__.py'), 'w') as f:
             f.write(init_body)
+    
+    def _get_distribute(self, dst_dir):
+        log.info(
+            'Getting "distribute_setup.py" from %r...'% DISTRIBUTE_SETUP_URL)
+        urllib.urlretrieve(
+            DISTRIBUTE_SETUP_URL,
+            os.path.join(dst_dir, 'distribute_setup.py'))
 
 
 def main():
