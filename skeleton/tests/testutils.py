4b4364d89ec9d69cec87aaa47afe3147a873939f
Damien Lebrun <dinoboff@hotmail.com>
2010-05-03 17:17:18 +0100

    Add skeleton.utils.insert_into_file()
    To add content into a file after a marker

---
+++ skeleton/tests/testutils.py
@@ -0,0 +1,74 @@
+"""
+Test in skeleton.utils.*
+"""
+from __future__ import with_statement
+
+import os
+import unittest
+
+from skeleton.tests.utils import TestCase
+from skeleton.utils import insert_into_file
+
+
+class TestInsertIntoFile(TestCase):
+    """Tests for in skeleton.utils.insert_into_file"""
+
+    def test_insert_into_file(self):
+        """test skeleton.insert_into_file()"""
+        target = os.path.join(self.tmp_dir.path, 'test.txt')
+        with open(target, 'w') as f_target:
+            f_target.write("""foo\n# -*- insert here -*- #\nbaz\n""")
+
+        insert_into_file(target, 'insert here', 'bar\n')
+        with open(target) as f_target:
+            self.assertEqual(
+                f_target.readlines(),
+                ['foo\n', '# -*- insert here -*- #\n', 'bar\n', 'baz\n']
+                )
+
+    def test_insert_with_indent(self):
+        """test skeleton.insert_into_file() with indent to keep"""
+        target = os.path.join(self.tmp_dir.path, 'test.txt')
+        with open(target, 'w') as f_target:
+            f_target.write("""foo\n  # -*- insert here -*- #\nbaz\n""")
+
+        insert_into_file(target, 'insert here', 'bar\n')
+        with open(target) as f_target:
+            self.assertEqual(
+                f_target.readlines(),
+                ['foo\n', '  # -*- insert here -*- #\n', '  bar\n', 'baz\n']
+                )
+
+    def test_insert_with_indent_to_lose(self):
+        """test skeleton.insert_into_file() with indent to lose"""
+        target = os.path.join(self.tmp_dir.path, 'test.txt')
+        with open(target, 'w') as f_target:
+            f_target.write("""foo\n  # -*- insert here -*- #\nbaz\n""")
+
+        insert_into_file(target, 'insert here', 'bar\n', keep_indent=False)
+        with open(target) as f_target:
+            self.assertEqual(
+                f_target.readlines(),
+                ['foo\n', '  # -*- insert here -*- #\n', 'bar\n', 'baz\n']
+                )
+
+    def test_insert_and_remove_marker(self):
+        """test skeleton.insert_into_file() with keep_marker off"""
+        target = os.path.join(self.tmp_dir.path, 'test.txt')
+        with open(target, 'w') as f_target:
+            f_target.write("""foo\n# -*- insert here -*- #\nbaz\n""")
+
+        insert_into_file(target, 'insert here', 'bar\n', keep_marker=False)
+        with open(target) as f_target:
+            self.assertEqual(
+                f_target.readlines(),
+                ['foo\n', 'bar\n', 'baz\n']
+                )
+
+def suite():
+    """Return tests for skeleton.utils.*  """
+    return unittest.TestLoader().loadTestsFromTestCase(TestInsertIntoFile)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- skeleton/utils.py
+++ skeleton/utils.py
@@ -1,3 +1,10 @@
+"""
+Skeleton Helpers
+"""
+
+from __future__ import with_statement
+from contextlib import closing
+import codecs
 import logging
 import optparse
 import os
@@ -6,7 +13,7 @@ import stat
 import sys
 
 
-VALID_OPTION_NAME = re.compile("[a-z]([\w\d]*[a-z0-9])?",re.IGNORECASE)
+VALID_OPTION_NAME = re.compile("[a-z]([\w\d]*[a-z0-9])?", re.IGNORECASE)
 
 def get_loggger(name):
     logger = logging.getLogger(name)
@@ -26,6 +33,75 @@ def vars_to_optparser(vars):
 
 def get_file_mode(path):
     stat.S_IMODE(os.stat(path)[stat.ST_MODE])
+
+def insert_into_file(
+    file_path, marker, text,
+    marker_tag="-*-", keep_indent=True, keep_marker=True, encoding="UTF-8"):
+    """
+    Insert text into file at specific markers.
+    
+    eg, for a file "test.txt" with::
+    
+        foo
+        -*- Insert Here -*-
+        baz
+    
+    `insert_into_file('test.txt', 'Insert Here', 'bar\n', keep_marker=False)`
+    would result in::
+    
+        foo
+        bar
+        baz
+    
+    @param file_path: file to insert content into
+    @type file_path: str
+    
+    @param marker: Marker too look for in the file
+    @type marker: str
+    
+    @param text: text to insert in the file
+    @type text: unicode
+    
+    @param marker_tag: text surrounding the marker
+    @type marker_tag: str
+    
+    @param keep_indent: Should it insert the text with the same marker indent.
+    @type keep_indent: bool
+    
+    @param keep_marker: Should the marker be removed
+    @type keep_marker: bool
+    
+    @param encoding: file encoding.
+    @type encoding: str
+    """
+    marker_pattern = re.escape('%s %s %s' % (marker_tag, marker, marker_tag,))
+    marker_re = re.compile(r"^(\s*).*%s.*$" % marker_pattern)
+    edited = False
+    new_content = []
+    with closing(codecs.open(file_path, 'r', encoding=encoding)) as opened_file:
+        for line in opened_file:
+            match = marker_re.match(line)
+            if match is None:
+                new_content.append(line)
+                continue
+
+            edited = True
+
+            if keep_marker:
+                new_content.append(line)
+
+            if keep_indent:
+                indent = match.groups()[0]
+                new_content.append('%s%s' % (indent, text,))
+            else:
+                new_content.append(text)
+
+    if not edited:
+        return
+
+    with closing(codecs.open(file_path, 'w', encoding=encoding)) as opened_file:
+        for line in new_content:
+            opened_file.write(line)
 
 
 class NullHandler(logging.Handler):
