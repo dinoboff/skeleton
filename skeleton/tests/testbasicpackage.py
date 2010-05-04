574a6be16c98260650503c70fb27641f27bca9f5
Damien Lebrun <dinoboff@hotmail.com>
2010-05-04 05:22:41 +0100

    Add a license template to basic package skeleton

--- skeleton/tests/testbasicpackage.py
+++ skeleton/tests/testbasicpackage.py
@@ -1,3 +1,7 @@
+"""
+Test skeleton.examples.basicpackage
+"""
+
 import os
 import unittest
 
@@ -6,50 +10,77 @@ from skeleton.tests.utils import TestCase
 
 
 class TestBasicPackage(TestCase):
+    """
+    Test the BasicPackage Skeleton
+    """
 
     def test_write(self):
+        """
+        Test skeleton.examples.basicpackage.BasicPackage with a single package
+        """
         # skip test on python 2.5
         if not hasattr('', 'format'):
             return
-        vars = {
+        variables = {
             'ProjectName': 'foo',
             'PackageName': 'foo',
             'Author': 'Damien Lebrun',
             'AuthorEmail': 'dinoboff@gmail.com',
             }
-        s = BasicPackage(vars)
-        s.write(self.tmp_dir.path)
+        skel = BasicPackage(variables)
+        skel.write(self.tmp_dir.path)
 
-        self.assertEqual(s['NSPackages'], [])
-        self.assertEqual(s['Packages'], ['foo'])
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'distribute_setup.py')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'MANIFEST.in')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'README.rst')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'setup.py')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'foo/__init__.py')))
+        self.assertEqual(skel['NSPackages'], [])
+        self.assertEqual(skel['Packages'], ['foo'])
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'distribute_setup.py')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'MANIFEST.in')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'README.rst')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'LICENSE')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'setup.py')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'foo/__init__.py')))
 
     def test_write_namespaces(self):
+        """
+        Test skeleton.examples.basicpackage.BasicPackage with namespaces
+        """
         # skip test on python 2.5
         if not hasattr('', 'format'):
             return
-        vars = {
+        variables = {
             'ProjectName': 'foo-bar-baz',
             'PackageName': 'foo.bar.baz',
             'Author': 'Damien Lebrun',
             'AuthorEmail': 'dinoboff@gmail.com',
             }
-        s = BasicPackage(vars)
-        s.write(self.tmp_dir.path)
+        skel = BasicPackage(variables)
+        skel.write(self.tmp_dir.path)
 
-        self.assertEqual(set(s['NSPackages']), set(['foo', 'foo.bar']))
-        self.assertEqual(set(s['Packages']), set(['foo', 'foo.bar', 'foo.bar.baz']))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'distribute_setup.py')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'MANIFEST.in')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'README.rst')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'setup.py')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'foo/__init__.py')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'foo/bar/__init__.py')))
-        self.assertTrue(os.path.exists(os.path.join(self.tmp_dir.path, 'foo/bar/baz/__init__.py')))
+        self.assertEqual(set(skel['NSPackages']), set(['foo', 'foo.bar']))
+        self.assertEqual(
+            set(skel['Packages']),
+            set(['foo', 'foo.bar', 'foo.bar.baz']))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'distribute_setup.py')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'MANIFEST.in')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'README.rst')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'LICENSE')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'setup.py')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'foo/__init__.py')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'foo/bar/__init__.py')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir.path, 'foo/bar/baz/__init__.py')))
 
     def test_main(self):
         """Test basicpackage.main() """
@@ -79,6 +110,9 @@ class TestBasicPackage(TestCase):
             ))
 
 def suite():
+    """
+    Return suite for skeleton.examples.basicpackage
+    """
     return unittest.TestLoader().loadTestsFromTestCase(TestBasicPackage)
 
 if __name__ == "__main__":
