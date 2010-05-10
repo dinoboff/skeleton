42abaca67b742ff343c4f6c5553b9eb9dad28d43
Damien Lebrun <dinoboff@hotmail.com>
2010-05-10 17:51:29 +0100

    Add insert_into_file to skeleton module

--- skeleton/__init__.py
+++ skeleton/__init__.py
@@ -2,10 +2,8 @@
 Basic Template system for project skeleton.
 
 skeleton is similar to the template part of PasteScript but 
-without any dependencies; it should also be compatible with Python 3.
-
-However in this early phase of development, it only target python 2.5+,
-and tests require Mock.
+without any dependencies.
 """
 
 from skeleton.core import Skeleton, Var
+from skeleton.utils import insert_into_file
